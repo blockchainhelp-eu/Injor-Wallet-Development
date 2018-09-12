9fdf014c5088f67e4862399684d9d8327c155db2
Blockchain Help <31084431+blockchainhelp@users.noreply.github.com>
2018-09-12 12:58:26 +0530

    Add files via upload

---
+++ kops_server.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+#  Copyright 2018 U.C. Berkeley RISE Lab
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os
+import subprocess
+import logging
+import zmq
+
+logging.basicConfig(filename='log.txt',level=logging.INFO)
+
+def run():
+    context = zmq.Context(1)
+    request_pull_socket = context.socket(zmq.PULL)
+    request_pull_socket.bind('tcp://*:7000')
+
+    while True:
+        msg = request_pull_socket.recv_string()
+        args = msg.split(':')
+
+        if args[0] == 'add':
+            num = int(args[1])
+            ntype = args[2]
+            logging.info('Adding %d new %s nodes...' % (num, ntype))
+
+            if ntype == 'memory':
+                resp = os.system('./add_nodes.sh ' + str(num) + ' 0 0 0')
+            else:
+                resp = os.system('./add_nodes.sh 0 ' + str(num) + ' 0 0')
+
+            if resp == 0:
+                logging.info('Successfully added %d %s nodes.' % (num, ntype))
+            else:
+                logging.error('Unexpected error while adding nodes.')
+        elif args[0] = 'remove':
+            ip = args[1]
+            ntype = args[2]
+            logging.info('Removing %s node with IP %s.' % (ntype, ip))
+
+            resp = os.system('./remove_node.sh %s %s' % (ntype, ip))
+
+            if resp == 0:
+                logging.info('Successfully removed nodes %s.' % (ip))
+            else:
+                logging.error('Unexpected error while removing node %s.' % (ip))
+
+
+
+run()
